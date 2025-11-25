315e60c1f0e0e5c0e315fe7dbfecf025e547f3cf
ohyeah521 <1449374+ohyeah521@users.noreply.github.com>
2025-11-26 07:39:10 +0800

    Add ssnotifyd to activated services

--- activated.sh
+++ activated.sh
@@ -186,6 +186,7 @@ PATCH_FILES=(
   "sbin/ssexechelperd"
   "sbin/ssroutined"
   "sbin/ssmessaged"
+  "sbin/ssnotifyd"
   "sbin/ssrtmpclientd"
   "webapi/Camera/src/SYNO.SurveillanceStation.Camera.so"
 )
